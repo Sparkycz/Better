778aff6ca7a3ad9e37703ce03f7ccedb4defe221
Vladimír Kašpar <vladimir.kaspar@heureka.cz>
2019-10-02 12:07:25 +0200

    Mute ES module in logs

--- better/app.py
+++ better/app.py
@@ -10,6 +10,7 @@ logging.basicConfig(
     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
 )
+logging.getLogger("elasticsearch").setLevel(logging.WARNING)
 
 
 def run(argv):
